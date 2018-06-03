a033c28bfc3b7250f1a294b6a19612f58c21521f
Lockal <lockal333@gmail.com>
2018-06-03 06:23:31 +0300

    test

---
+++ main.js
@@ -0,0 +1,5 @@
+const acc = document.querySelector('.map-info__btn');
+
+const handleClick = () => document.querySelector('.map-info__menu').style.display = "flex";
+
+acc.addEventListener('click', handleClick)
